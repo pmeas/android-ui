005998417ec847b6942efebc01c07470a0ac3c4d
mattfg <matt.gaertner@yahoo.com>
2017-03-23 14:22:04 -0400

    Added THE MODEL for effects. Still needs testing. This is also a breaking change from the previous unfinished version of THE MODEL, so I need to go back and fix callsites.

--- app/src/main/java/zeinhijazi/com/pmeas/effects/Effect.java
+++ app/src/main/java/zeinhijazi/com/pmeas/effects/Effect.java
@@ -1,50 +1,60 @@
 package zeinhijazi.com.pmeas.effects;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 /**
  * Created by zhijazi on 3/11/17.
  */
 
 public class Effect {
-
-    private String effectName;
-    private ArrayList<String> parameters;
-    private ArrayList<Float> values;
-
-    public Effect() {
-        effectName = "";
-        parameters = new ArrayList<>();
-        values = new ArrayList<>();
+    private EffectsDefaults.EffectTypes type;
+    private Map<String, ?> params = new HashMap<>();
+    public Effect(EffectsDefaults.EffectTypes type) {
+        this.type = type;
     }
 
-    public Effect(String effectName, ArrayList<String> parameters, ArrayList<Float> values) {
-        this.effectName = effectName;
-        this.parameters = parameters;
-        this.values = values;
+    static boolean isComplex(EffectsDefaults.EffectTypes type, String paramJsonName)
+        throws NoSuchElementException
+    {
+        for (EffectsDefaults.EffectDefaults effect : type.defaults) {
+            if(effect.name.equals(paramJsonName)) {
+                return effect.isComplex;
+            }
+        }
+        throw new NoSuchElementException("Key "+paramJsonName+" does not exist in "+type.displayName+"'s list of params");
     }
 
-    public String getEffectName() {
-        return effectName;
+    static boolean hasParam(EffectsDefaults.EffectTypes type, String paramJsonName)
+    {
+        for (EffectsDefaults.EffectDefaults effect : type.defaults) {
+            if(effect.name.equals(paramJsonName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void setEffectName(String effectName) {
-        this.effectName = effectName;
+    int getSimpleParam(String paramJsonName)
+        throws IllegalStateException
+    {
+        if(isComplex(type, paramJsonName)) {
+            throw new IllegalStateException(type.displayName+" is a complex type");
+        }
+        else {
+            return (Integer) params.get(paramJsonName);
+        }
     }
 
-    public ArrayList<String> getParameters() {
-        return parameters;
-    }
-
-    public void setParameters(ArrayList<String> parameters) {
-        this.parameters = parameters;
-    }
-
-    public ArrayList<Float> getValues() {
-        return values;
-    }
-
-    public void setValues(ArrayList<Float> values) {
-        this.values = values;
+    Collection<Integer> getComplexParam(String paramJsonName)
+        throws IllegalStateException
+    {
+        if(isComplex(type, paramJsonName)) {
+            return (Collection<Integer>) params.get(paramJsonName);
+        } else {
+            throw new IllegalStateException(type.displayName+" is a simple type");
+        }
     }
 }
