cfb12a70f642a05926caed162d8437b877c88b57
Hijizzle <hijazizein2014@gmail.com>
2017-03-24 00:16:49 -0400

    ListView implementation of Effect Adding
    The UI now has action bar buttons that allow for sending and adding of
    effects. Sending is currently nonfunctional.
    
    Adding an effect consists of opening a dialog window to choose the
    effect.

--- app/src/main/java/zeinhijazi/com/pmeas/EffectsActivity.java
+++ app/src/main/java/zeinhijazi/com/pmeas/EffectsActivity.java
@@ -1,9 +1,18 @@
 package zeinhijazi.com.pmeas;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,10 +27,13 @@ public class EffectsActivity extends AppCompatActivity
     ArrayList<Effect> effects;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_effects);
+
+        ArrayList<String> param = new ArrayList<>();
+        param.add("Feedback");
+        param.add("Slope");
 
         effects = new ArrayList<>();
         effects.add(new Effect(EffectsDefaults.EffectTypes.DISTORTION));
@@ -32,5 +44,98 @@ public class EffectsActivity extends AppCompatActivity
 
         effectsListView = (ListView)findViewById(R.id.enabledEffectsList);
         effectsListView.setAdapter(listAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.pmeasbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_send:
+                Toast.makeText(this, "Send Button Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_add:
+                Toast.makeText(this, "Add Button Selected", Toast.LENGTH_SHORT).show();
+                final CharSequence effectsNames[] = new CharSequence[] {"Distortion", "Delay", "Reverb", "Chorus", "Frequency Shift", "Harmonizer", "Flanger", "Phaser"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Choose an Effect");
+                builder.setItems(effectsNames, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.out.print("ADD EFFECT CLICK INDEX: " + which +", EFFECT NAME: " + effectsNames[which]);
+                        ArrayList<String> effectParameters = new ArrayList<>();
+                        switch (which) {
+                            case 0:
+                                // stuff
+                                effectParameters.add("Distortion");
+                                effectParameters.add("Tone");
+                                break;
+                            case 1:
+                                // stuff
+                                effectParameters.add("Delay Time");
+                                effectParameters.add("Feedback");
+                                break;
+                            case 2:
+                                // stuff
+                                effectParameters.add("Balance");
+                                effectParameters.add("Tone");
+                                effectParameters.add("Delay");
+                                effectParameters.add("Room Size");
+                                break;
+                            case 3:
+                                // stuff
+                                effectParameters.add("Balance");
+                                effectParameters.add("Rate");
+                                effectParameters.add("Depth");
+                                break;
+                            case 4:
+                                // stuff
+                                effectParameters.add("Pitch");
+                                break;
+                            case 5:
+                                // stuff
+                                effectParameters.add("Feedback");
+                                effectParameters.add("Shift");
+                                break;
+                            case 6:
+                                // stuff
+                                effectParameters.add("Depth");
+                                effectParameters.add("Frequency");
+                                effectParameters.add("Feedback");
+                                break;
+                            case 7:
+                                // stuff
+                                effectParameters.add("Frequency");
+                                effectParameters.add("Spread");
+                                effectParameters.add("Feedback");
+                                effectParameters.add("Num");
+                                break;
+                            default:
+                                break;
+                        }
+
+                        // TODO: Instead of going through the listarray for all values, you can just
+                        // TODO: store the created effects in some global variable that we can then read.
+                        // TODO: But then there is issue with looking at what values are... so maybe listview only way.
+                        // TODO: Also we maybe dont need the FLoats arraylist in an effect if we can read raw seekbar values.
+                        String selectedEffect = effectsNames[which].toString();
+                        effects.add(new Effect(selectedEffect, effectParameters, new ArrayList<Float>()));
+
+                        listAdapter.notifyDataSetChanged();
+
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/zeinhijazi/com/pmeas/util/EnabledListAdapter.java
+++ app/src/main/java/zeinhijazi/com/pmeas/util/EnabledListAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -25,7 +26,6 @@ public class EnabledListAdapter extends ArrayAdapter<Effect> {
 
     private ArrayList<Effect> enabledEffects;
 
-    private RelativeLayout relativeLayout;
     private LayoutInflater layoutInflater;
 
 
@@ -47,26 +47,52 @@ public class EnabledListAdapter extends ArrayAdapter<Effect> {
         if(view == null) {
             view = layoutInflater.inflate(R.layout.enabled_listview, parent, false);
 
-            relativeLayout = (RelativeLayout)view.findViewById(R.id.layout_id);
-
             Effect currentEffect = enabledEffects.get(position);
+            ArrayList<String> params = currentEffect.getParameters();
 
             TextView effectName = (TextView)view.findViewById(R.id.enabled_effect_name);
-            Collection<String> params = currentEffect.getParamNames();
-            effectName.setText(currentEffect.getDisplayName());
+            effectName.setText(currentEffect.getEffectName());
 
-            for(String param: params) {
-                TextView paramName = new TextView(context);
-                paramName.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                paramName.setText(param);
-                relativeLayout.addView(paramName);
+            System.out.println("ARRAY ADAPTER EFFECT NAME RECEIVED: " + currentEffect.getEffectName());
+
+            //TODO: Make only one variable and just change the assignment into different ViewbyIds
+            switch (params.size()) {
+                case 4:
+                    TextView fourthParam = (TextView)view.findViewById(R.id.fourthParamName);
+                    fourthParam.setText(params.get(3));
+                    fourthParam.setVisibility(View.VISIBLE);
+
+                    SeekBar fourthBar = (SeekBar)view.findViewById(R.id.fourthParamSlider);
+                    fourthBar.setVisibility(View.VISIBLE);
+                case 3:
+                    TextView thirdParam = (TextView)view.findViewById(R.id.thirdParamName);
+                    thirdParam.setText(params.get(2));
+                    thirdParam.setVisibility(View.VISIBLE);
+
+                    SeekBar thirdBar = (SeekBar)view.findViewById(R.id.thirdParamSlider);
+                    thirdBar.setVisibility(View.VISIBLE);
+                case 2:
+                    // do stuff
+                    TextView secondParam = (TextView)view.findViewById(R.id.secondParamName);
+                    secondParam.setText(params.get(1));
+                    secondParam.setVisibility(View.VISIBLE);
+
+                    SeekBar secondBar = (SeekBar)view.findViewById(R.id.secondParamSlider);
+                    secondBar.setVisibility(View.VISIBLE);
+                case 1:
+                    // do stuff
+                    TextView firstParam = (TextView)view.findViewById(R.id.firstParamName);
+                    firstParam.setText(params.get(0));
+                    firstParam.setVisibility(View.VISIBLE);
+
+                    SeekBar firstBar = (SeekBar)view.findViewById(R.id.firstParamSlider);
+                    firstBar.setVisibility(View.VISIBLE);
+                    break;
+                default:
+                    break;
             }
 
 
-
-//            Button btnTag = new Button(context);
-//            btnTag.setText("Lmao");
-//            relativeLayout.addView(btnTag);
 
         }
 
