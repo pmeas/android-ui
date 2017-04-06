214d0b146f439fad7a6810fa12de6361a5527fbc
zhijazi <hijazizein2014@gmail.com>
2017-04-06 11:20:27 -0400

    Clamp the effect paramters to be within the range as specified by the design document (and thus, the backend).
    Slight modification of min and max parmater values to allow for clamping to ranges.
    Change how the slider value textview calculates the progress (which is where the actual clamping happens.)
    Create field data for divideFactor to allow us to actually clamp the values due to restrictions with SeekBar.

--- app/src/main/java/zeinhijazi/com/pmeas/effects/EffectsDefaults.java
+++ app/src/main/java/zeinhijazi/com/pmeas/effects/EffectsDefaults.java
@@ -10,12 +10,14 @@ public class EffectsDefaults {
         final String name;
         final String jsonName;
         final boolean isComplex;
+        final int divideFactor;
 
-        protected EffectDefaults(String name, String jsonName, boolean isComplex)
+        protected EffectDefaults(String name, String jsonName, boolean isComplex, int divideFactor)
         {
             this.name = name;
             this.jsonName = jsonName;
             this.isComplex = isComplex;
+            this.divideFactor = divideFactor;
         }
 
         public String getName() {
@@ -29,14 +31,18 @@ public class EffectsDefaults {
         public boolean isComplex() {
             return this.isComplex;
         }
+
+        public int getDivideFactor() {
+            return this.divideFactor;
+        }
     }
     public static class SimpleEffectDefaults
             extends EffectDefaults
     {
         int min, max, defaultValue;
 
-        public SimpleEffectDefaults(String name, String jsonName, int min, int max, int defaultValue) {
-            super(name, jsonName, false);
+        public SimpleEffectDefaults(String name, String jsonName, int min, int max, int defaultValue, int divideFactor) {
+            super(name, jsonName, false, divideFactor);
             this.min = min;
             this.max = max;
             this.defaultValue = defaultValue;
@@ -59,8 +65,8 @@ public class EffectsDefaults {
     {
         int min, max, defaultValue;
 
-        public ComplexEffectDefaults(String name, String jsonName, int min, int max, int defaultValue) {
-            super(name, jsonName, true);
+        public ComplexEffectDefaults(String name, String jsonName, int min, int max, int defaultValue, int divideFactor) {
+            super(name, jsonName, true, divideFactor);
             this.min = min;
             this.max = max;
             this.defaultValue = defaultValue;
@@ -81,56 +87,56 @@ public class EffectsDefaults {
     public static enum EffectTypes {
         CHORUS("Chorus", "chorus",
                 new SimpleEffectDefaults("Balance", "balance",
-                        0, 100, 50),
+                        0, 100, 50, 100),
                 new SimpleEffectDefaults("Feedback", "feedback",
-                        0, 100, 50),
+                        0, 100, 50, 100),
                 new ComplexEffectDefaults("Depth", "depth",
-                        0, 500, 250)),
+                        0, 500, 250, 100)),
         DELAY("Delay", "delay",
                 new SimpleEffectDefaults("Feedback", "feedback",
-                        0, 100, 50),
+                        0, 100, 50, 100),
                 new ComplexEffectDefaults("Delay Time (ms)", "delay",
-                        0, 100, 50)),
+                        0, 1000, 50, 100)),
         DISTORTION("Distortion", "distortion",
                 new SimpleEffectDefaults("Balance", "drive",
-                        0, 100, 50),
+                        0, 100, 50, 100),
                 new SimpleEffectDefaults("Tone", "slope",
-                        0, 100, 50)),
+                        0, 100, 50, 100)),
         FLANGER(Math.random() > 0.05? "Flanger":"Flanders", "flanger",
                 new SimpleEffectDefaults("Depth", "depth",
-                        0, 100, 0),
+                        0, 100, 0, 100),
                 new SimpleEffectDefaults("Frequency", "freq",
-                        0, 100, 0),
+                        0, 100, 0, 100),
                 new SimpleEffectDefaults("Feedback", "feedback",
-                        0, 100, 0)),
+                        0, 100, 0, 100)),
         FREQUENCY_SHIFT("Frequency Shift", "freqshift",
                 new ComplexEffectDefaults("Shift", "shift",
-                        0, Integer.MAX_VALUE, 0)),
+                        0, 500, 0, 1)),
         HARMONIZER("Harmonizer", "harmonizer",
                 new SimpleEffectDefaults("Feedback", "feedback",
-                        0, 100, 50),
+                        0, 100, 50, 100),
                 new ComplexEffectDefaults("Shift", "transpose",
-                        -20, 20, 0)),
+                        -20, 40, 0, 1)),
         PHASER("Phaser", "phaser",
                 new SimpleEffectDefaults("Frequency", "frequency",
-                        0, Integer.MAX_VALUE, 0),
+                        0, 5000, 0, 1),
                 new SimpleEffectDefaults("Spread", "spread",
-                        0, Integer.MAX_VALUE, 0),
+                        0, 100, 11, 100),
                 new SimpleEffectDefaults("Q-Factor", "q",
-                        0, Integer.MAX_VALUE, 0),
+                        0, 50, 0, 1),
                 new SimpleEffectDefaults("Feedback", "feedback",
-                        0, 100, 0),
+                        0, 100, 0, 100),
                 new SimpleEffectDefaults("Num", "num",
-                        0, Integer.MAX_VALUE, 0)),
+                        0, 50, 8, 1)),
         REVERB("Reverb", "reverb",
                 new SimpleEffectDefaults("Balance", "balance",
-                        0, 100, 50),
+                        0, 100, 50, 100),
                 new SimpleEffectDefaults("Tone", "cutoff",
-                        0, Integer.MAX_VALUE, 0),
+                        0, 35000, 3500, 1),
                 new SimpleEffectDefaults("Delay", "revtime",
-                        0, Integer.MAX_VALUE, 0),
+                        0, 100, 0, 100),
                 new SimpleEffectDefaults("Room Size", "roomsize",
-                        25, 400, 50));
+                        25, 375, 50, 100));
 
         EffectDefaults[] defaults;
         String displayName;
